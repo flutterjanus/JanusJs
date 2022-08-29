cec20c10e8037dba3b2feb7ca0d4b293f795697f
shivanshtalwar <shivansh@utopia.tech>
2022-08-29 10:50:22 +0300

    build

--- dist/wrapper_plugins/sip.d.ts
+++ dist/wrapper_plugins/sip.d.ts
@@ -3,6 +3,46 @@ import { JanusPlugin } from "../janus_plugin";
 import { JanusSession } from "../janus_session";
 export declare class JanusSipPlugin extends JanusPlugin {
     constructor(instance: Janus, session: JanusSession, handle: PluginHandle, controllers: Controllers);
-    boomSip(): void;
+    register(username: string, options: {
+        type?: "guest" | "helper";
+        send_register?: boolean;
+        force_udp?: boolean;
+        force_tcp?: boolean;
+        sips?: boolean;
+        rfc2543_cancel?: boolean;
+        secret?: string;
+        ha1_secret?: string;
+        authuser?: string;
+        display_name?: string;
+        user_agent?: string;
+        proxy?: string;
+        outbound_proxy?: string;
+        headers?: any;
+        contact_params?: any;
+        incoming_header_prefixes?: string[];
+        refresh?: boolean;
+        master_id?: string;
+        register_ttl?: number;
+    }): Promise<void>;
+    call(uri: string, options?: {
+        call_id?: string;
+        refer_id?: string;
+        headers?: any;
+        srtp?: string;
+        srtp_profile?: string;
+        autoaccept_reinvites?: boolean;
+        secret?: string;
+        ha1_secret?: string;
+        authuser?: string;
+    }, offer?: RTCSessionDescription): Promise<void>;
+    decline(code?: number, headers?: any): Promise<void>;
+    hangup(headers?: any): Promise<void>;
+    accept(options?: {
+        srtp?: string;
+        headers: any;
+        autoaccept_reinvites: boolean;
+    }): Promise<void>;
+    hold(direction: "sendonly" | "recvonly" | "inactive"): Promise<void>;
+    unhold(): Promise<void>;
 }
 //# sourceMappingURL=sip.d.ts.map
